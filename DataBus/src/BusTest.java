005bc9013b0d925eb2f58900140f55947c1fdd5f
ral2bp <laszlo.ravasz@hu.bosch.com>
2016-09-27 13:40:26 +0200

    Extension of bus for example and better understanding

--- DataBus/src/BusTest.java
+++ DataBus/src/BusTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,28 +22,12 @@ public class BusTest {
 
         first.setACCMainSwitchState(true);
 
-        assertEquals(first == second, true);
+        assertEquals(second.getACCMainSwitchState(), true);
     }
 
     @org.junit.Test
     public void doesClassExist() throws Exception{
         assert(bus.getClass() != null);
-    }
-
-    @org.junit.Test
-    public void hasBusDataMemberACCMainSwitchState(){
-        //bus.ACCMainSwitchState = false;
-        //assertEquals(bus.ACCMainSwitchState,false);
-    }
-
-    @org.junit.Test
-    public void ACCMainSwitchGetterReturnsSetValue(){
-       // bus.ACCMainSwitchState = false;
-        bus.setACCMainSwitchState(false);
-        assertEquals(bus.getACCMainSwitchState(), false);
-        bus.setACCMainSwitchState(true);
-        //bus.ACCMainSwitchState = true;
-        assertEquals(bus.getACCMainSwitchState(), true);
     }
 
     @org.junit.Test
@@ -52,8 +38,78 @@ public class BusTest {
         assertEquals(bus.getACCMainSwitchState(), true);
     }
 
+    @Test
+    public void gasPedalGetterReturnsSetterGivenValue(){
+        bus.setGasPedal(65);
+        assertEquals(bus.getGasPedal(), 65);
+    }
+
+    @Test
+    public void gasPedalValueIsMaximum100() {
+        bus.setGasPedal(111);
+        assertEquals(bus.getGasPedal(), 100);
+    }
+
+    @Test
+    public void gasPedalValueIsMinimum0() {
+        bus.setGasPedal(-77);
+        assertEquals(bus.getGasPedal(), 0);
+    }
+
+    @Test
+    public void brakePedalGetterReturnsSetterGivenValue(){
+        bus.setBrakePedal(65);
+        assertEquals(bus.getBrakePedal(), 65);
+    }
+
+    @Test
+    public void brakePedalValueIsMaximum100() {
+        bus.setBrakePedal(111);
+        assertEquals(bus.getBrakePedal(), 100);
+    }
+
+    @Test
+    public void brakePedalValueIsMinimum0() {
+        bus.setBrakePedal(-77);
+        assertEquals(bus.getBrakePedal(), 0);
+    }
+
+    @Test
+    public void steeringWheelAngleGetterReturnsSetterGivenValue(){
+        bus.setSteeringWheelAngle(65);
+        assertEquals(bus.getSteeringWheelAngle(), 65);
+    }
+
+    @Test
+    public void steeringWheelAngleValueIsMaximum100() {
+        bus.setSteeringWheelAngle(111);
+        assertEquals(100,bus.getSteeringWheelAngle());
+    }
+
+    @Test
+    public void steeringWheelAngleValueIsMinimum0() {
+        bus.setSteeringWheelAngle(-77);
+        assertEquals(bus.getSteeringWheelAngle(), 0);
+    }
+
+    @Test
+    public void directionIndicatorLeftActiveGetterReturnsSetterGivenValue(){
+        bus.setDirectionIndicatorLeftActive(true);
+        assertEquals(bus.isDirectionIndicatorLeftActive(), true);
+        bus.setDirectionIndicatorLeftActive(false);
+        assertEquals(bus.isDirectionIndicatorLeftActive(), false);
+    }
+
+    @Test
+    public void gearPositionGetterReturnsSetterGivenValue(){
+        bus.setGearPosition(Bus.GearPosition.NEUTRAL);
+        assertEquals(Bus.GearPosition.NEUTRAL, bus.getGearPosition());
+        bus.setGearPosition(Bus.GearPosition.DRIVE);
+        assertEquals(Bus.GearPosition.DRIVE, bus.getGearPosition());
+    }
+
     @org.junit.After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception { 
         /*GCC will remove bus*/
     }
 
