841eed62889d0bef2b20604d86587a4d67e9700d
nkaroly91 <nemethkaroly91@gmail.com>
2016-09-30 21:15:30 +0200

    - Nincs kimondott haladás, sak nézem, hogy hogy működik a git az intellij-ben - Az előző commit-ból kimaradt, hogy az object osztály átneveztem WorldObject-re, nehogy összekeverjük a legősibb Object osztállyal (prog2 anyag)

--- DataBus/src/Environment/XMLParserMain.java
+++ DataBus/src/Environment/XMLParserMain.java
@@ -1,7 +1,13 @@
 package Environment;
+import Environment.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ral2bp on 2016.09.29..
  */
 public class XMLParserMain {
+
+    List<WorldObject> DynamicObjects = new ArrayList<WorldObject>();
 }
