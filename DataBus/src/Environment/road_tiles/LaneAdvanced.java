6cb51fa6d33a7145aebaef8e079b3cb99bddddd2
SutoZ <suto.zoli92@gmail.com>
2016-10-02 23:54:47 +0200

    LanseSimple, LanceAdvanced have now constructors.

--- DataBus/src/Environment/road_tiles/LaneAdvanced.java
+++ DataBus/src/Environment/road_tiles/LaneAdvanced.java
@@ -1,5 +1,6 @@
 package Environment.road_tiles;
 
+import Environment.Position;
 import Environment.WorldObject;
 
 /**
@@ -11,4 +12,9 @@ public class LaneAdvanced extends WorldObject {
     public enum RoadPaintings3 {undefinied1,undefinied2}
     public enum LaneAdvancedType {CrossRoads,Rotary,TJunctionLeft,TJunctionRight}
 
+    public LaneAdvanced(int Id, String Name, int Type, Position startPosition,
+                        int[] Transform, int Zlevel, int Opacity) {
+        super(Id, Name, Type, startPosition,
+        Transform, Zlevel, Opacity);
+    }
 }
--- DataBus/src/Environment/road_tiles/LaneSimple.java
+++ DataBus/src/Environment/road_tiles/LaneSimple.java
@@ -1,5 +1,6 @@
 package Environment.road_tiles;
 
+import Environment.Position;
 import Environment.WorldObject;
 
 /**
@@ -10,4 +11,10 @@ public class LaneSimple extends WorldObject{
     public enum RoadPaintings2 {undefinied1,undefinied2,undefinied3,undefinied4,undefinied5,undefinied6,undefinied7,undefinied8}
     public enum RoadPaintings3 {undefinied1,undefinied2,undefinied3}
     public enum LaneSimpleType {Left45,Left65,Left90,Right45,Right65,Right90,Straight}
+
+    public LaneSimple(int Id, String Name, int Type, Position startPosition,
+                      int[] Transform, int Zlevel, int Opacity) {
+            super(Id, Name, Type, startPosition,
+            Transform, Zlevel, Opacity);
+    }
 }
