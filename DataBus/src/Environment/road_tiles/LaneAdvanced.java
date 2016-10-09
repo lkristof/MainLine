dbae29884b6693a655f0390424cd2e936a16a1dd
nkaroly91 <nemethkaroly91@gmail.com>
2016-10-09 16:35:15 +0200

    Elvileg elkészült az XML feldogozás!!! Kéretik ellenőrizni nem-e maradt-e ki valami!!!!

--- DataBus/src/Environment/road_tiles/LaneAdvanced.java
+++ DataBus/src/Environment/road_tiles/LaneAdvanced.java
@@ -6,14 +6,27 @@ import Environment.WorldObject;
  * Created by nemet on 2016. 09. 30..
  */
 public class LaneAdvanced extends WorldObject {
-    public enum RoadPaintings1 {undefinied1,undefinied2}
-    public enum RoadPaintings2 {undefinied1,undefinied2}
-    public enum RoadPaintings3 {undefinied1,undefinied2}
+    public enum RoadPaintings1 {rp_none,rp_sep_1_s}
+    public enum RoadPaintings2 {rp_none,rp_sep_2_s}
+    public enum RoadPaintings3 {rp_none,rp_sep_3_s}
     public enum LaneAdvancedType {CrossRoads,Rotary,TJunctionLeft,TJunctionRight}
 
-    public LaneAdvanced(int Id, int[] startPosition,
-                        double[] Transform, int Zlevel, int Opacity) {
-        super(Id, startPosition,
-        Transform, Zlevel, Opacity);
+    RoadPaintings1 roadPaintings1;
+    RoadPaintings2 roadPaintings2;
+    RoadPaintings3 roadPaintings3;
+    LaneAdvancedType laneAdvancedType;
+    int[] roadColor1;
+    int[] roadColor2;
+    int[] roadColor3;
+
+    public LaneAdvanced(int Id, int[] startPosition, double[] Transform, int Zlevel, int Opacity, int[] roadColor1, int[] roadColor2, int[] roadColor3, RoadPaintings1 roadPaintings1, RoadPaintings2 roadPaintings2, RoadPaintings3 roadPaintings3, LaneAdvancedType laneAdvancedType) {
+        super(Id, startPosition, Transform, Zlevel, Opacity);
+        this.roadPaintings1 = roadPaintings1;
+        this.roadPaintings2 = roadPaintings2;
+        this.roadPaintings3 = roadPaintings3;
+        this.laneAdvancedType = laneAdvancedType;
+        this.roadColor1 = roadColor1;
+        this.roadColor2 = roadColor2;
+        this.roadColor3 = roadColor3;
     }
 }
--- DataBus/src/Environment/road_tiles/LaneSimple.java
+++ DataBus/src/Environment/road_tiles/LaneSimple.java
@@ -2,18 +2,20 @@ package Environment.road_tiles;
 
 import Environment.WorldObject;
 
+import java.util.Arrays;
+
 /**
  * Created by nemet on 2016. 09. 30..
  */
 public class LaneSimple extends WorldObject{
-    public enum RoadPaintings1 {undefinied1,undefinied2,undefinied3}
-    public enum RoadPaintings2 {undefinied1,undefinied2,undefinied3,undefinied4,undefinied5,undefinied6,undefinied7,undefinied8}
-    public enum RoadPaintings3 {undefinied1,undefinied2,undefinied3}
+    public enum RoadPaintings1 {rp_none,rp_sep_1_s,rp_sep_1_sb}
+    public enum RoadPaintings2 {rp_none,rp_sep_2_bnd,rp_sep_2_d,rp_sep_2_dbnd,rp_sep_2_ds,rp_sep_2_s,rp_sep_2_sd,rp_sep_2_ss}
+    public enum RoadPaintings3 {rp_none,rp_sep_3_s,rp_sep_3_sb}
     public enum LaneSimpleType {Left45,Left65,Left90,Right45,Right65,Right90,Straight}
 
-    String[] roadColor1;
-    String[] roadColor2;
-    String[] roadColor3;
+    int[] roadColor1;
+    int[] roadColor2;
+    int[] roadColor3;
 
     RoadPaintings1 roadPaintings1;
     RoadPaintings2 roadPaintings2;
@@ -21,7 +23,7 @@ public class LaneSimple extends WorldObject{
     LaneSimpleType laneSimpleType;
 
     public LaneSimple(int Id, int[] startPosition,
-                      double[] Transform, int Zlevel, int Opacity, String[] roadColor1, String[] roadColor2, String[] roadColor3, RoadPaintings1 roadPaintings1, RoadPaintings2 roadPaintings2,
+                      double[] Transform, int Zlevel, int Opacity, int[] roadColor1, int[] roadColor2, int[] roadColor3, RoadPaintings1 roadPaintings1, RoadPaintings2 roadPaintings2,
                       RoadPaintings3 roadPaintings3, LaneSimpleType laneSimpleType) {
         super(Id, startPosition, Transform, Zlevel, Opacity);
         this.roadPaintings1 = roadPaintings1;
@@ -35,6 +37,6 @@ public class LaneSimple extends WorldObject{
 
     @Override
     public String toString() {
-        return roadPaintings1.toString() + " " + roadPaintings2.toString() + " " + roadPaintings3.toString() + " " + laneSimpleType.toString();
+        return  "RoadColor1: " + Arrays.toString(roadColor1) + " " + "RoadColor2: " + Arrays.toString(roadColor2) + " " + "RoadColor3: " + Arrays.toString(roadColor3) + " " + roadPaintings1.toString() + " " + roadPaintings2.toString() + " " + roadPaintings3.toString() + " " + laneSimpleType.toString();
     }
 }
