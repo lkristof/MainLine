98bc4e4be33ff9a78cec50cf9c76af636158b7bd
SutoZ <suto.zoli92@gmail.com>
2016-10-22 15:21:51 +0200

    Interface for the needed positions of the sensors from the teams.

---
+++ DataBus/src/Environment/IGetPosition.java
@@ -0,0 +1,15 @@
+package Environment;
+
+/**
+ * Created by Zolee on 2016. 10. 22..
+ */
+public interface IGetPosition {
+    //Mitro Team
+    void getCamera();
+
+    //FJKKL Team
+    void getUltraSound();
+
+    //HUB csapat
+    void getRadar();
+}
